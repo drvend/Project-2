ce700df536a5bc9f61a77442141b36fb75a57b61
snprim <sprim@ncsu.edu>
2020-10-20 00:53:11 -0400

    added secondary analysis

--- Renderfile.R
+++ Renderfile.R
@@ -15,5 +15,5 @@ reports <- tibble(outputfilenames, params)
 
 apply(reports, MARGIN = 1,
       FUN = function(day){
-        render(input = "C:/Users/drven/Documents/ST 558/Project-2/ONPReport.Rmd", output_file = day[[1]], params = day[[2]])
+        render(input = "ONPReport.Rmd", output_file = day[[1]], params = day[[2]])
       })
